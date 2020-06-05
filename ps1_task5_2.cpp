abf8e53ed483e7d0c97004143281a0b5553488b8
FoysalRoni <56961940+FoysalRoni@users.noreply.github.com>
2020-06-05 16:30:06 +0600

    created ps1_task5_2.cpp
    Efficient Fiboonacci code added

---
+++ ps1_task5_2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int fibo(int n)
+{
+    int i,fib[n];
+
+    fib[0] = 0;
+    fib[1] = 1;
+
+
+    for(i=2; i<=n; i++)
+    {
+        fib[i] = fib[i-1]+fib[i-2];
+    }
+    return fib[n];
+}
+
+
+
+
+int main()
+{
+    int n,number;
+
+    cin >> n;
+
+    number = fibo(n);
+
+    cout << number << endl;
+
+}
