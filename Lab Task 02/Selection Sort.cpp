c2628754c3ca1e30e51214fe776b96fcc7b92944
FoysalRoni <56961940+FoysalRoni@users.noreply.github.com>
2020-06-13 21:58:48 +0600

    created selectionsort.cpp

---
+++ Lab Task 02/Selection Sort.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void selection_sort(int arr[],int n)
+{
+    int i,j,min,temp;
+    for(i=0; i<n-1; i++)
+    {
+        min = i;
+        for(j=i+1; j<n; j++)
+        {
+            if(arr[j]<arr[min])
+            {
+                min = j;
+            }
+        }
+        if(min!=i)
+        {
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
+
+int main()
+{
+    int n,i;
+    cin>>n;
+    int arr[n];
+    for(i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    selection_sort(arr,n);
+    for(i=0; i<n; i++)
+    {
+        cout << arr[i]<<" ";
+    }
+    cout<<endl;
+}
